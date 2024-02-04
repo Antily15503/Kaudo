f8f77ef687f2090f66371f25b6cb8e3852994638
Antily15503 <yitian.li@warriorlife.net>
2024-02-03 22:45:22 -0600

    Finished Introduction section, as well as tweaked some parameters in App.css

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -5,13 +5,23 @@ const Hero = () => {
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center w-full mb-10 pt-5">
         <img src= {logo} alt="kaudo_logo"
-        className= "w-36 object-contain"/>
+        className= "w-36 object-contain"/>  
 
         <button type="button" onClick={() => window.open('https://github.com/Antily15503/Kaudo')}
         className="black_btn">
           Github
         </button>
       </nav>
+
+      <h1 className="head_text">
+        Introducing Summarizer
+      </h1>
+      <h1 className="desc">
+      Learn what you need to know immediately and eliminate any clutter through Summarizer by Kaudo, an open-source article summarizer that turns lengthy articles into clear and easy-to-digest summaries.
+      </h1>
+      <h4 className="orange_gradient font-semibold">
+        Powered by OpenAI GPT-4
+      </h4>
     </header>
   )
 }
